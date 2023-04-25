f54b9feae260c67e29cbacba241c62a42f5985cb
yuna <yunae426@gmail.com>
2023-04-25 11:15:53 +0900

    static routing

---
+++ note/src/pages/products/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ProductsPage() {
+    return (
+        <h1>
+           제품 소개 페이지! 
+        </h1>
+    );
+}
+
---
+++ note/src/pages/products/pants.tsx
@@ -0,0 +1,8 @@
+export default function Pants() {
+    return (
+        <h1>
+            바지!
+        </h1>
+    );
+}
+
